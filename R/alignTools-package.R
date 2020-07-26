4a26cb3cb667085960a4d26921d3a140531b0100
Omar Daniel Leon Alvarado <leon.alvarado12@gmail.com>
2020-07-25 21:10:04 -0300

    Add files via upload

---
+++ R/alignTools-package.R
@@ -0,0 +1,23 @@
+#'
+#' @title alignTools
+#'
+#' @description easy download and manage GenBank data, and alignments for phylogenetics
+#' 
+#' @name alignTools-package
+#'
+#' @aliases alignTools
+#'
+#' @docType package
+#'
+#' @author Omar Daniel Leon-Alvarado <leon.alvarado12@@gmail.com>
+#'
+#' @import ape
+#'
+#' @import phangorn
+#'
+#' @includeRmd vignettes/alignTools.rmd
+#'
+#' @keywords packagelibrary
+#'
+ function()
+NULL
